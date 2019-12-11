86a1cdd7f48068e6bae030d23b801727c8078337
ava <dev@computerfox.xyz>
2019-12-11 08:38:02 -0500

    litepub object properly overloads @context to default litepub schema

--- litepub/core.lisp
+++ litepub/core.lisp
@@ -1,7 +1,10 @@
 (in-package :clap-litepub)
 
 (defclass litepub-object (clap-entities:ap-object)
-  ((sensitive :accessor sensitive :initarg :sensitive) ;; as:sensitive
+  ((@context :accessor atcontext
+	     :initarg :atcontext
+	     :initform "https://litepub.social/litepub/context.jsonld")
+   (sensitive :accessor sensitive :initarg :sensitive) ;; as:sensitive
    (atom-uri :accessor atom-uri :initarg :atom-uri) ;; ostatus:atomUri
    (in-reply-to-atom-uri :accessor in-reply-to-atom-uri
 			 :initarg :in-reply-to-atom-uri) ;; ostatus:inReplyToAtomUri
